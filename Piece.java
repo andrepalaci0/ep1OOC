98d42972b0d0b45e586a9446d92d453ec94f3ec3
Ana Beatriz R. Chagas <121564402+anabeatrizchagas@users.noreply.github.com>
2023-06-12 18:25:58 -0300

    modified:   Piece.java

--- Piece.java
+++ Piece.java
@@ -2,13 +2,23 @@ package ep1OOC;
 /**
  * Classe que contém informações das peças de jogo
  */
+
 public class Piece {
+
     /**
      * Construtor que define a cor e o tipo da peça
      * @param color Cor da peça
      * @param isMaster Se o tipo da peça é mestre ou não
      */
-    public Piece(Color color, boolean isMaster) {
+
+    private Color color;
+    private boolean isMaster;
+
+
+    public Piece(Color color, boolean isMaster) 
+    {
+        this.color = color;
+        this.isMaster = isMaster;
 
     }
 
@@ -16,15 +26,22 @@ public class Piece {
      * Método que devolve a cor da peça
      * @return Enum Color com a cor da peça
      */
-    public Color getColor() {
-        return null;
+
+    public Color getColor() 
+    {
+        return this.color;
+
     }
 
     /**
      * Método que devolve se é um mestre ou não
      * @return Booleano true para caso seja um mestre e false caso contrário
      */
-    public boolean isMaster() {
-        return false;
+
+    public boolean isMaster() 
+    {
+        return this.isMaster;
+
     }
+
 }
