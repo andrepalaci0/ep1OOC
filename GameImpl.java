0f7066c2fd1165614d5323a658b7e11393ad3f21
palaci0 <palaci0@pop-os.localdomain>
2023-06-17 17:46:54 -0300

    resolves Spot.createBoard() problems / introduces Main for testing / Currently working funcs: all the gameImpl constructors, getblue and red Player, checkVictory, printBoard, getTableCard

--- GameImpl.java
+++ GameImpl.java
@@ -5,7 +5,7 @@ public class GameImpl implements Game {
     private Player player1, player2;
     private Card tableCard;
     private Card cards[];
-    private Spot board[][] = new Spot[5][5];
+    private Spot board[][];
     private int roundCounter = 0;
 
     /**
@@ -15,7 +15,7 @@ public class GameImpl implements Game {
     public GameImpl() {
 
         this.cards = Card.createCards();
-        this.board = initializeBoard(board);
+        this.board = Spot.createBoard();
         this.player1 = new Player("redPlayer", Color.RED, this.cards[1], this.cards[3]);
         this.player2 = new Player("bluePlayer", Color.BLUE, this.cards[0], this.cards[2]);
         this.tableCard = this.cards[4];
@@ -34,7 +34,7 @@ public class GameImpl implements Game {
     public GameImpl(String nomeVermelho, String nomeAzul) { // tem que conferir a questão das cartas, p ver qual jogador
                                                             // começa
         this.cards = Card.createCards();
-        this.board = initializeBoard(board);
+        this.board = Spot.createBoard();
         this.player1 = new Player(nomeVermelho, Color.RED, this.cards[1], this.cards[3]);
         this.player2 = new Player(nomeAzul, Color.BLUE, this.cards[0], this.cards[2]);
         this.tableCard = this.cards[4];
@@ -56,7 +56,7 @@ public class GameImpl implements Game {
                 throw new InvalidCardException("Invalid Card Deck: deck has empty spaces");
         }
         this.cards = newDeck;
-        this.board = initializeBoard(board);
+        this.board = Spot.createBoard();
         this.player1 = new Player(nomeVermelho, Color.RED, this.cards[1], this.cards[3]);
         this.player2 = new Player(nomeAzul, Color.BLUE, this.cards[0], this.cards[2]);
         this.tableCard = this.cards[4];
@@ -237,13 +237,16 @@ public class GameImpl implements Game {
         boolean enemMasterAlive = false;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) { // tenta encontrar o master inimigo
-                if (this.board[i][j].getPiece().isMaster() &&
-                        this.board[i][j].getPiece().getColor() == enemColor)
-                    enemMasterAlive = true;
+                if (this.board[i][j].getPiece() != null) {
+                    if (this.board[i][j].getPiece().isMaster() && this.board[i][j].getPiece().getColor() == enemColor)
+                        enemMasterAlive = true;
+                }
             }
         }
-        if (!enemMasterAlive)
+        if (!enemMasterAlive) {
+            System.out.println("ta caindo aqui");
             return true; // se não encontrado o master inimigo, aconteceu a vitoria
+        }
         if (enemColor == Color.BLUE) { // confere se peças inimigas não estão no templo
             if (this.board[0][2].getPiece() == null)
                 return false;
@@ -299,38 +302,38 @@ public class GameImpl implements Game {
      * OBS: Esse método é opcional não será utilizado na correção, mas serve para
      * acompanhar os resultados parciais do jogo
      */
+    public void printTest()
+    {
+        System.out.println(board[4][2].getColor());
+    }
+
     public void printBoard() {
 
         for (int i = 0; i < board.length; i++) {
 
             for (int j = 0; j < board.length; j++) {
 
-                if (board[i][j].getPiece().isMaster()) {
+                if (board[i][j].getPiece() == null) {
+                    System.out.print("00 ");
+
+                } else if (board[i][j].getPiece().isMaster()) {
 
                     if (board[i][j].getPiece().getColor() == Color.BLUE)
                         System.out.print("BM ");
                     if (board[i][j].getPiece().getColor() == Color.RED)
                         System.out.print("RM ");
 
-                }
-
-                if (board[i][j].getPiece() != null) {
+                } else if (board[i][j].getPiece() != null) {
 
                     if (board[i][j].getPiece().getColor() == Color.BLUE)
                         System.out.print("BP ");
                     if (board[i][j].getPiece().getColor() == Color.RED)
                         System.out.print("RP ");
 
-                } else {
-
-                    System.out.print("00 ");
-
                 }
 
             }
-
             System.out.println();
-
         }
 
     }
@@ -343,56 +346,6 @@ public class GameImpl implements Game {
      *         vazios
      */
 
-    private Spot[][] initializeBoard(Spot board[][]) {
-
-        for (int i = 0; i < board.length; i++) {
-
-            Piece auxPiece;
-            boolean isMaster = false;
-
-            for (int j = 0; j < board.length; j++) {
-
-                Position auxPos = new Position(i, j);
-
-                if (i == 0) { // pecas azuis
-
-                    Color auxColor = Color.BLUE;
-                    if (j == 2) {
-                        isMaster = true;
-                        auxPiece = new Piece(auxColor, isMaster);
-                        board[i][j] = new Spot(auxPiece, auxPos, auxColor);
-                    } else {
-                        auxPiece = new Piece(auxColor, isMaster);
-                        board[i][j] = new Spot(auxPiece, auxPos);
-                    }
-
-                }
-
-                if (i == 4) { // pecas vermelhas
-
-                    Color auxColor = Color.RED;
-                    if (j == 2) {
-                        isMaster = true;
-                        auxPiece = new Piece(auxColor, isMaster);
-                        board[i][j] = new Spot(auxPiece, auxPos, auxColor);
-                    } else {
-                        auxPiece = new Piece(auxColor, isMaster);
-                        board[i][j] = new Spot(auxPiece, auxPos);
-                    }
-
-                }
-
-                // lugares vazios
-                board[i][j] = new Spot(auxPos);
-
-            }
-
-        }
-
-        return board;
-
-    }
-
+    
     // fim da classe
-
 }
