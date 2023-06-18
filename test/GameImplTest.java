436dd1138922affdeb28f0c7bbe4b660105cb66f
Ana Beatriz R. Chagas <121564402+anabeatrizchagas@users.noreply.github.com>
2023-06-18 10:49:44 -0300

    new file:   .vscode/settings.json new file:   test/GameImplTest.java

---
+++ test/GameImplTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import ep1OOC.GameImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+
+public class GameImplTest {
+
+
+    
+}
