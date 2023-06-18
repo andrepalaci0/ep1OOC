77a30df4e8cceeb343aca9111b70895f2a2d09d1
palaci0 <palaci0@pop-os.localdomain>
2023-06-18 15:20:08 -0300

    improves test main

--- main.java
+++ main.java
@@ -2,7 +2,20 @@ package ep1OOC;
 
 public class main{
     public static void main(String[] args) {
-        GameImpl gameImpl = new GameImpl("Vermelho", "Azul");
+
+        Card deck[] = new Card[5]; 
+        //new Card("tiger", Color.BLUE, new Position[]{new Position(1, 0), new Position(-2, 0)});
+        //new Card("crab", Color.BLUE, new Position[]{new Position(0, -2), new Position(-1, 0), new Position(0, 2)});
+        //new Card("dragon", Color.RED, new Position[]{new Position(1, -1), new Position(1, 1), new Position(-1, -2), new Position(-1, 2)});
+       
+        deck [0] = new Card("goose", Color.BLUE, new Position[]{new Position(-1, -1), new Position(0, -1), new Position(0, 1), new Position(1, 1)});
+        deck [1] = new Card("rabbit", Color.BLUE, new Position[]{new Position(1, -1), new Position(-1, 1), new Position(0, 2)});
+        deck [2] = new Card("elephant", Color.RED, new Position[]{new Position(-1, -1), new Position(0, -1), new Position(-1, 1), new Position(0, 1)});
+        deck [3] = new Card("frog", Color.RED, new Position[]{new Position(-1, -1), new Position(0, -2), new Position(1, 1)});
+        deck [4] = new Card("rooster", Color.RED, new Position[]{new Position(0, -1), new Position(0, 1), new Position(1, -1), new Position(-1, 1)});
+
+
+        GameImpl gameImpl = new GameImpl("Vermelho", "Azul", deck);
         System.out.println(gameImpl.getRedPlayer().getName());
         System.out.println(gameImpl.getBluePlayer().getName());
         //Card cards[] = gameImpl.getBluePlayer().getCards();
@@ -15,19 +28,29 @@ public class main{
         //    }
         //}
         gameImpl.printBoard();
-        gameImpl.printTest();
+        //gameImpl.printTest();
         System.out.println(gameImpl.checkVictory(Color.NONE));
-        System.out.println(gameImpl.getTableCard().getName());
-        System.out.println(gameImpl.getTableCard().getColor());
-
-        Player bluePlayer = gameImpl.getBluePlayer();
-        Card[] cards = bluePlayer.getCards();
-        Card card1 = cards[0];
-        System.out.println(card1.getName());
-        Position[] positions = cards[0].getPositions();
+        //System.out.println(gameImpl.getTableCard().getName());
+        System.out.println("TABLE CARD COLOR:"+ gameImpl.getTableCard().getColor());
+        System.out.println("TABLE CARD: " + gameImpl.getTableCard().getName());
+        Player redPlayer = gameImpl.getRedPlayer();
+        Card redCards[] = redPlayer.getCards();
+        for (int i = 0; i < redCards.length; i++) {
+            System.out.println("RED CARDS: "+ redCards[i].getName());
+        }
+        Position[] positions = deck[3].getPositions();
+        //System.out.println(deck[3].getName());
         Position auxPosition = positions[0];
-        Position ogPosition = new Position(0, 2);
-        gameImpl.makeMove(card1, auxPosition, ogPosition);
+        Position ogPosition = new Position(4, 2);
+        
+        gameImpl.makeMove(deck[3], auxPosition, ogPosition);
         gameImpl.printBoard();
+        System.out.println("TABLE CARD: " + gameImpl.getTableCard().getName());
+
+        //Position testPosition1 = new Position(3,1);
+        //gameImpl.makeMove(deck[3], auxPosition, testPosition1);
+        Player bluePlayer = gameImpl.getBluePlayer();
+        Card blueCards[] = gameImpl.getBluePlayer().getCards();
+
     }
 }
